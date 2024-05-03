c1e89f53563d1f568b31d2ca3508f9392e73c194
matias acosta <acosta_matias8@hotmail.com>
2024-05-02 21:16:00 -0300

    first commit

---
+++ src/constants/consts.ts
@@ -0,0 +1,63 @@
+const path = '/assets/img/'
+export  const technologies = [
+    {   
+        name: 'Angular',
+        icon: `${path}angular.svg`
+    }, 
+    {
+        name: 'Javascript',
+        icon: `${path}javascript.svg`
+    },
+    {
+        name: 'HTML',
+        icon: `${path}html5.svg`
+    },
+    {
+        name: 'CSS',
+        icon: `${path}css.svg`
+    },
+    {
+        name: 'NodeJS',
+        icon: `${path}nodejs.svg`
+    },
+    {
+        name: 'NestJS',
+        icon: `${path}nestjs.svg`
+    },
+    {
+        name: 'RabbitMQ',
+        icon: `${path}rabbitmq.svg`
+    },
+    {
+        name: 'MongoDB',
+        icon: `${path}mongodb.svg`
+    },
+    {
+        name: 'PostgreSQL',
+        icon: `${path}postgresql.svg`
+    }
+
+]
+
+export const socials = [
+    {
+        name: 'Linkedin',
+        icon: `${path}linkedin.svg`,
+        url: 'https://www.linkedin.com/in/matias-acosta-5a50041b5/'
+    },
+    {
+        name: 'Github',
+        icon: `${path}github.svg`,
+        url: 'https://github.com/MatuAcosta?tab=repositories'
+    },
+    {
+        name: 'Gmail',
+        icon: `${path}gmail.svg`,
+        url: 'mailto:matiaseacosta18@gmail.com'
+    },
+    {
+        name: 'Download CV',
+        icon: `${path}cv.svg`,
+        url: `cv_acostamatias.pdf`
+    }
+]
