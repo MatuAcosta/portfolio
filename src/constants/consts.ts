5b53871e03ddd522f758cb4f26a378532176c2c5
matias acosta <acosta_matias8@hotmail.com>
2024-05-07 16:39:55 -0300

    agregando cabañasibera

--- src/constants/consts.ts
+++ src/constants/consts.ts
@@ -87,7 +87,7 @@ export const projects = [
         colortitleStyle: 'text-lime-800',
         name: 'Cabañas Iberá - A website for a rural tourism business',
         description: 'Cabañas Iberá is a website for a rural tourism business located in the Iberá Wetlands, Corrientes, Argentina.',
-        url: '',
+        url: 'https://cabanias-ibera.pages.dev/home',
         github: '',
         src: '/assets/img/cabanasibera.png',
         tecnologies: [{
